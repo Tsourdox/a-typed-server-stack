c45fbb2a64eb852932e8a0fae60c6b8db91cdc28
David Jensen <Tsourdox@icloud.com>
2020-02-22 18:41:58 +0100

    upates to database structure

--- server/database.ts
+++ server/database.ts
@@ -1,34 +1,42 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Db } from 'mongodb'
 
 const databaseName = "mongo"
 const url = 'mongodb://localhost:27017/mongo'
-const settings = { useUnifiedTopology: true }
+const options = { useUnifiedTopology: true }
 
-const collections = ['users']
+const collections = ['users', 'meta']
+let db: Db
 
 /**
  * Will test connection with the database server and create
  * the database and all collections if they do not exist.
  */
 export async function connectWithDatabase() {
-    console.log('Connecting to database...')
-    MongoClient.connect(url, settings, function(err, client) {
-        if (err) throw err
-        
-        // Connect to or create the database
-        var db = client.db(databaseName)
-        console.log("Connection established with database: " + databaseName)
-        
-        // Create all collections that has been defined!
-        for (const name of collections) {
-            db.createCollection(name, function(err) {
-                if (err) throw err
-                console.log(`Collection ${name} created!`)
+    console.log('\nConnecting to database...')
+    try {
+        // Connect to or create the database if not present
+        const client = await new MongoClient(url, options)
+        const connection = await client.connect()
+        db = await connection.db(databaseName)
+        console.log(`Connection established with database: "${databaseName}"`)
 
-                // db.collection(name).insertOne({ username: 'tsourdox', password: 'beta' })
-                
-                client.close()
-            })
-        }
-    })
+        // Create collections if not present
+        createCollections()
+
+    } catch(err) {
+        throw err
+    }
 }
+
+/** Create all missing collections */
+async function createCollections() {
+    console.log('\nChecking collections...')
+    for (const name of collections) {
+        await db.createCollection(name)
+        console.log(`"${name}"\t ✅`)
+    }
+}
+
+// export async function insert() {
+//     await db.collection(name).insertOne({ username: 'tsourdox', password: 'beta' })
+// }
