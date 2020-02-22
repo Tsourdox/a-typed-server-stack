076c293475b7c195af23e74bb2160ae2fe4fd42e
David Jensen <Tsourdox@icloud.com>
2020-02-22 17:02:33 +0100

    add 404 "page"

--- server/server.ts
+++ server/server.ts
@@ -9,16 +9,23 @@ const port = 4000
 /** Keeps track on all visitors */
 let count = 0
 
-// Defined Routes
+//======  Defined Routes  ======//
+// Counting middleware
 app.get('/', (...[, , next]) => {
     console.log('Someone asked to view the home page ^^, visitor: ', ++count)
     next()
 })
+
+// Serve public folder
 app.use(express.static('public'))
 
+// 404 page
+app.use((...[, res]) => res.status(404).send(
+    "I'm just here to tell you that the page does not exist, have a great day ^^"
+))
 
 
-// Spin up the server!
+//======  Spin up the server!  ======//
 app.listen(port, host, () => {
     console.log(`Sever is listening on http://${host}:${port}`)
     
